39c5a1d5f7940ef33d96a3b7c1456fd1373f7a84
kumo01GitHub <kumo01@outlook.com>
2026-06-20 16:14:37 +0900

    feat: assumption

--- src/poussins/tactics/derived.py
+++ src/poussins/tactics/derived.py
@@ -1,6 +1,9 @@
 """
 Derived tactics.
 """
+from .primitive import exact
+from ..ast import Formula
+from ..errors import TacticError
 from ..kernel import ProofEngine
 from ..tactics import intro
 
@@ -8,3 +11,15 @@ from ..tactics import intro
 def intros(proof_engine: ProofEngine, hyp_names: list[str]):
     for hyp_name in hyp_names:
         intro(proof_engine, hyp_name)
+
+def assumption(proof_engine: ProofEngine, formula: Formula):
+    current_goal = proof_engine.state.current_goal
+    if current_goal is None:
+        raise TacticError("No active goal to apply assumption tactic.")
+
+    for k, v in current_goal.context.hyps.items():
+        if v == formula:
+            exact(proof_engine, k)
+            return
+
+    raise TacticError("No matching hypothesis found in the current context.")
