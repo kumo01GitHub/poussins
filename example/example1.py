eacb8f916b1923bcfe6f44bb26841ad45a5bde89
kumo01GitHub <kumo01@outlook.com>
2026-06-20 17:09:22 +0900

    fix: rename

--- example/example1.py
+++ example/example1.py
@@ -6,10 +6,10 @@ from poussins import Lemma, Prop, Environment
 
 a, b = Prop("A"), Prop("B")
 
-example_and = Lemma("Example1", a >> (b >> (a & b)))
+example1 = Lemma("Example1", a >> (b >> (a & b)))
 
-example_and.intros(["ha", "hb"])
-example_and.constructor()
-example_and.exact("ha")
-example_and.exact("hb")
-example_and.qed(Environment())
+example1.intros(["ha", "hb"])
+example1.constructor()
+example1.exact("ha")
+example1.exact("hb")
+example1.qed(Environment())
